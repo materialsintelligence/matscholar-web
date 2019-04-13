f9548aacd34fadd6257a0b8ac8b2db4e36db925a
Amalie Trewartha <amalie.trewartha@gmail.com>
2019-04-12 20:58:20 -0700

    Added search app

--- matscholar_web/view/search_app.py
+++ matscholar_web/view/search_app.py
@@ -2,92 +2,57 @@ import dash_html_components as html
 import dash_materialsintelligence as dmi
 import dash_core_components as dcc
 from dash.dependencies import Input, Output, State
+import json, os, random
+from matscholar import Rester
 
 VALID_FILTERS = ["material", "property", "application", "descriptor", "characterization", "synthesis", "phase"]
+FILTERS_TO_KEYS = {"material": "MAT", "property":"PRO", "application": "APL", "synthesis": "SMT", "characterization": "CMT", "descriptor": "DSC", "phase": "SPL"}
+KEYS_TO_FILTERS = {v: k for k,v in FILTERS_TO_KEYS.items()}
+with open(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'static','data','entities.json'),'r') as f:
+    entities_dict = json.load(f)
+PLACEHOLDERS = {x: entities_dict[FILTERS_TO_KEYS[x]][0]+", "+entities_dict[FILTERS_TO_KEYS[x]][1]+", ..." for x in VALID_FILTERS}
+rester = Rester()
 
-def search_html():
-    return html.Div([
-        html.Div(dcc.Input(
-            id="search_input",
-            type="text",
-            autofocus=True,
-            placeholder="Text search...",
-            style={"width": "100%"}),
-            style={"display": "table-cell", "width": "100%"}),
-        html.Div(html.Button(
-            "Search",
-            className="button-search",
-            id="search_btn"),
-            style={"display": "table-cell", "verticalAlign": "top", "paddingLeft": "10px"})],
-            className="row", style={"display": "table", "marginTop": "10px"}
-        )
+def format_result(result):
+    return html.Div([dcc.Markdown("[%s](https://doi.org/%s)" %(result['doi'],result['doi']))]+
+        [html.Label(KEYS_TO_FILTERS[k]+": "+", ".join([v for v in result[k]])) for k,v in result.items() if k != 'doi'],style={"padding": 0})
+
+def results_html(results):
+    return html.Div([format_result(r) for r in results],style={"padding": 5})
 
 def filters_html(label, placeholder):
     return html.Div([
-        html.Label('Add filters for {}'.format(label)),
+        html.Label('{}:'.format(label)),
          dcc.Dropdown(
-            options=[
-                {'label': 'sputtering', 'value': 'sputtering'},
-                {'label': 'sintering', 'value': 'sintering'},
-                {'label': 'MOCVD', 'value': 'MOCVD'}
-            ],
+            options=[{'label': x, 'value': x} for x in entities_dict[FILTERS_TO_KEYS[label]]],
+            id=label+'-filters'
+            ,
         multi=True,
         placeholder=placeholder
         ),
     ],
-    style={'width':'50%'}
+    style={'padding':5}
 )
 
 def serve_layout():
-    return html.Div([search_html(),
-                     html.Div(html.Button(
-                         'Material',
-                         id='filters-btn'
-                     )),
-                     html.Div("", id='filters')])
+    buttons = [html.Div(html.Button(
+                         "Search",
+                         id='search-btn',
+                         name="Search"
+                     ),style={'textAlign':'center'})]
+    buttons.append(html.Div(html.Label("Filters")))
+    for f in VALID_FILTERS:
+        buttons.append(filters_html(f,PLACEHOLDERS[f]))
+    layout = [html.Div(buttons,style={'width': '20%', 'float': 'left', 'display': 'inline-block'}),html.Div(id='results',style={'width': '70%', 'float': 'right', 'display': 'inline-block'})]
+    return html.Div(layout)
 
 def bind(app):
     @app.callback(
-        Output('filters', 'children'),
-        [Input('filters-btn', 'n_clicks')])
-    def show_filters(n_clicks):
-        if n_clicks is not None:
-            return filters_html('material', 'Al2O3, alumina ...')
-
-
-# def serve_layout(path=None):
-#     if path is not None and len(path) > len("/search"):
-#         path = path[len("/search")+1::]
-#         path = path.replace("%20", " ")
-#     options = []
-#     value = options
-#     if path is not None:
-#         filter_value_material = path.split("/")
-#         if len(filter_value_material) == 3:
-#             options = [{'label': 'material:' + filter_value_material[2],
-#                         'value': 'material:' + filter_value_material[2]},
-#                        {'label': filter_value_material[0] + ':' + filter_value_material[1],
-#                         'value': filter_value_material[0] + ':' + filter_value_material[1]}]
-#             value = options
-#     return html.Div([search_html(),
-#         html.Div(dmi.DropdownCreatable(
-#         options=options,
-#         multi=True,
-#         promptText="Add filter ",
-#         className="search-filters",
-#         placeholder="filter:value1,value2",
-#         value=value,
-#         id='search_filters'),
-#     ),
-#     html.Div(
-#         'Valid filters: ' + ', '.join(VALID_FILTERS),
-#         style={"color": "grey", "padding": "10px 1px", "fontSize": "10pt"},
-#         className="row"),
-#     html.Div(html.Button(
-#         "Generate summary",
-#         className="button-search",
-#         id="search_btn"),
-#         style={"display": "table-cell", "verticalAlign": "top", "paddingLeft": "10px"}),
-#     html.Div("", id="search_results", className="row"),
-#
-#     ])
+        Output('results', 'children'),
+        [Input('search-btn','n_clicks')],
+        [State(f+'-filters', 'value') for f in VALID_FILTERS])
+    def show_results(*args,**kwargs):
+        if list(args)[0] is not None:
+            filters = {f: list(args)[i+1] for i,f in enumerate(VALID_FILTERS) if list(args)[i+1] is not None}
+            results = rester.search_ents(filters)
+            return results_html(results)
