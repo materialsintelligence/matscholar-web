f3d0a428fb035d1cb34b93944fd6736dcce8c33a
Amalie Trewartha <amalie.trewartha@gmail.com>
2019-05-08 14:29:33 -0700

    Fixed a bug where search app would be confused by leading or trailing whitespace

--- matscholar_web/callbacks/search_callbacks.py
+++ matscholar_web/callbacks/search_callbacks.py
@@ -64,6 +64,6 @@ def bind(app):
     def show_results(*args,**kwargs):
         if list(args)[0] is not None:
             text = str(args[1])
-            filters = {f: args[i+2].split(', ') for i,f in enumerate(VALID_FILTERS) if ((list(args)[i+2] is not None) and (args[i+2].split(', ') != ['']))}
+            filters = {f: [s.strip() for s in args[i+2].split(',')] for i,f in enumerate(VALID_FILTERS) if ((list(args)[i+2] is not None) and (args[i+2].split(',') != ['']))}
             results = rester.search_text_with_ents(text,filters)
             return results_html(results)
