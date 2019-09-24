84a1a21b5790e4cd7bb3340f177355f6cf01c820
ardunn <ardunn@lbl.gov>
2019-09-23 19:52:39 -0700

    remove bind

--- matscholar_web/callbacks/analysis_callbacks.py
+++ matscholar_web/callbacks/analysis_callbacks.py
@@ -1,4 +1,3 @@
-
 import dash_html_components as html
 import string
 import os
@@ -6,12 +5,14 @@ import json
 import urllib
 import numpy as np
 from dash.dependencies import Input, Output, State
-from matscholar_web.base import rester, valid_entity_filters, highlight_mapping, TIMEOUT
+
+from matscholar_web.app import app, cache
+from matscholar_web.base import rester
 
 # Get the rester and random docs on import
 local_dir = os.path.dirname(__file__)
 with open(os.path.join(local_dir, "../static/data/sample_docs.json"), "r") as f:
-  sample_docs = json.load(f)
+    sample_docs = json.load(f)
 
 label_mapping = {
     "MAT": "Material",
@@ -26,83 +27,87 @@ label_mapping = {
 
 
 def highlight_entities(tagged_doc):
-  highlighted_doc = []
-  tagged_doc = [(token, tag) for sent in tagged_doc[0]
-                for (token, tag) in sent]
-  for idx, (token, tag) in enumerate(tagged_doc):
-    if idx < len(tagged_doc) - 1:
-      next_token_punct = True if tagged_doc[idx +
-                                            1][0] in string.punctuation else False
-    else:
-      next_token_punct = False
-    span = html.Span(token,
-                     className="highlighted {}".format(tag),
-                     style={"padding-right": "0px" if next_token_punct else "4px",
-                            "background-clip": "content-box"})
-    highlighted_doc.append(span)
-  return highlighted_doc
+    highlighted_doc = []
+    tagged_doc = [(token, tag) for sent in tagged_doc[0]
+                  for (token, tag) in sent]
+    for idx, (token, tag) in enumerate(tagged_doc):
+        if idx < len(tagged_doc) - 1:
+            next_token_punct = True if tagged_doc[idx +
+                                                  1][
+                                           0] in string.punctuation else False
+        else:
+            next_token_punct = False
+        span = html.Span(token,
+                         className="highlighted {}".format(tag),
+                         style={
+                             "padding-right": "0px" if next_token_punct else "4px",
+                             "background-clip": "content-box"})
+        highlighted_doc.append(span)
+    return highlighted_doc
 
 
 def get_labels():
-  return [html.Span(label_mapping[key],
-                    className="highlighted {}".format(key),
-                    style={"padding-right": "10px", "background-clip": "content-box"})
-          for key in label_mapping]
+    return [html.Span(label_mapping[key],
+                      className="highlighted {}".format(key),
+                      style={"padding-right": "10px",
+                             "background-clip": "content-box"})
+            for key in label_mapping]
 
 
-### Extract App Callbacks ###
+# Extract app callbacks
 @app.callback(
-  Output("extract-highlighted", "children"),
-  [Input("extract-button", "n_clicks")],
-  [State("extract-textarea", "value"),
-   State("normalize-radio", "value")])
+    Output("extract-highlighted", "children"),
+    [Input("extract-button", "n_clicks")],
+    [State("extract-textarea", "value"),
+     State("normalize-radio", "value")])
 def highlight_extracted(n_clicks, text, normalize):
     if n_clicks is not None:
-      # Extract highlighted
-      return_type = "normalized" if normalize == "yes" else "concatenated"
-      result = rester.get_ner_tags([text], return_type=return_type)
-      tagged_doc = result["tags"]
-      relevance = result["relevance"][0]
-      highlighted = highlight_entities(tagged_doc)
+        # Extract highlighted
+        return_type = "normalized" if normalize == "yes" else "concatenated"
+        result = rester.get_ner_tags([text], return_type=return_type)
+        tagged_doc = result["tags"]
+        relevance = result["relevance"][0]
+        highlighted = highlight_entities(tagged_doc)
 
-      # Add the warning
-      if not relevance:
-        warning = "WARNING!!! Our classifier has flagged this document as not relevant" \
-                  " to inorganic materials science. Expect lower than optimum performance."
-      else:
-        warning = ""
+        # Add the warning
+        if not relevance:
+            warning = "WARNING!!! Our classifier has flagged this document as not relevant" \
+                      " to inorganic materials science. Expect lower than optimum performance."
+        else:
+            warning = ""
 
-      # Update download link
-      doc = {"sentences": []}
-      for sent in tagged_doc[0]:
-        new_sent = []
-        for token, tag in sent:
-          new_sent.append({
-              "token": token,
-              "tag": tag
-          })
-        doc["sentences"].append(new_sent)
-      json_string = json.dumps(doc)
-      json_string = "data:text/csv;charset=utf-8," + \
-          urllib.parse.quote(json_string)
-      return html.Div([html.Div(html.Label("Extracted Entity Tags:")),
-                       html.Div(warning, style={
-                                "padding-bottom": "20px", "color": "red"}),
-                       html.Div(highlighted),
-                       html.Div(html.Label("Labels"), style={
-                                "padding-top": "15px"}),
-                       html.Div(get_labels()),
-                       html.Div(html.A("Download entities as json",
-                                       id="entity-download-link",
-                                       href=json_string,
-                                       download="tagged_docs.json",
-                                       target="_blank"),
-                                style={"padding-top": "15px"})])
+        # Update download link
+        doc = {"sentences": []}
+        for sent in tagged_doc[0]:
+            new_sent = []
+            for token, tag in sent:
+                new_sent.append({
+                    "token": token,
+                    "tag": tag
+                })
+            doc["sentences"].append(new_sent)
+        json_string = json.dumps(doc)
+        json_string = "data:text/csv;charset=utf-8," + \
+                      urllib.parse.quote(json_string)
+        return html.Div([html.Div(html.Label("Extracted Entity Tags:")),
+                         html.Div(warning, style={
+                             "padding-bottom": "20px", "color": "red"}),
+                         html.Div(highlighted),
+                         html.Div(html.Label("Labels"), style={
+                             "padding-top": "15px"}),
+                         html.Div(get_labels()),
+                         html.Div(html.A("Download entities as json",
+                                         id="entity-download-link",
+                                         href=json_string,
+                                         download="tagged_docs.json",
+                                         target="_blank"),
+                                  style={"padding-top": "15px"})])
 
-  @app.callback(
-      Output('extract-textarea', 'value'),
-      [Input("extract-random", 'n_clicks')])
-  def get_random(n_clicks):
-    if n_clicks is not None:
-      return np.random.choice(sample_docs)
-    return ""
+
+    @app.callback(
+        Output('extract-textarea', 'value'),
+        [Input("extract-random", 'n_clicks')])
+    def get_random(n_clicks):
+        if n_clicks is not None:
+            return np.random.choice(sample_docs)
+        return ""
--- matscholar_web/index.py
+++ matscholar_web/index.py
@@ -1,61 +1,26 @@
 import os
-from os import environ
 
-import dash
-import dash_auth
 import dash_html_components as html
 from dash.dependencies import Input, Output
 from flask import send_from_directory
-from flask_caching import Cache
 
+from matscholar_web.app import app, cache
 from matscholar_web.view import search_view, analysis_view
 from matscholar_web.footer import get_footer
 from matscholar_web.header import get_header
 from matscholar_web.nav import get_nav
-from matscholar_web.callbacks import search_view_callbacks, analysis_callbacks
+
 """
 Declarations for the core dash app.
 """
 
-"""
-APP CONFIG
-"""
-
-app = dash.Dash(
-    meta_tags=[
-        {
-            "name": "viewport",
-            "content": "width=device-width, initial-scale=1"
-        }
-    ]
-)
-app.css.config.serve_locally = True
-app.scripts.config.serve_locally = True
-app.config.suppress_callback_exceptions = True
-app.title = "matscholar - rediscover materials"
-cache = Cache(app.server, config={'CACHE_TYPE': 'simple'})
-
-# Authentication
-VALID_USERNAME_PASSWORD_PAIRS = [
-    [environ['MATERIALS_SCHOLAR_WEB_USER'],
-     environ['MATERIALS_SCHOLAR_WEB_PASS']]]
-auth = dash_auth.BasicAuth(
-    app,
-    VALID_USERNAME_PASSWORD_PAIRS
-)
-
 stylesheet = html.Link(rel='stylesheet', href='/static/css/bulma.css')
 stylesheet_div = html.Div(stylesheet, className="container is-hidden")
-
-"""
-VIEW
-"""
 
 app_container = html.Div("", id="app_container")
 footer = get_footer()
 header = get_header()
 nav = get_nav()
-
 app.layout = html.Div(
     [
         stylesheet_div,
@@ -66,10 +31,6 @@ app.layout = html.Div(
     ],
     className="container"
 )
-
-"""
-CALLBACKS
-"""
 
 
 # callbacks for loading different apps
@@ -89,10 +50,6 @@ def display_page(path, search):
 def get_stylesheet(path):
     static_folder = os.path.join(os.getcwd(), 'matscholar_web/static/css')
     return send_from_directory(static_folder, path)
-
-
-search_view_callbacks.bind(app, cache)
-analysis_callbacks.bind(app)
 
 
 # setting the static path for robots.txt
