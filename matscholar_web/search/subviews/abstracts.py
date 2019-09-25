23764a0e468dbd6031ad231917501a810d3d5380
ardunn <ardunn@lbl.gov>
2019-09-24 23:00:42 -0700

    entities close to good formatting on results

--- matscholar_web/search/subviews/abstracts.py
+++ matscholar_web/search/subviews/abstracts.py
@@ -11,19 +11,14 @@ from matscholar_web.search.util import parse_search_box, \
 
 MAX_N_ABSTRACTS = 100
 
-label_mapping = {
-    "material": "MAT_summary",
-    "application": "APL_summary",
-    "property": "PRO_summary",
-    "phase": "SPL_summary",
-    "synthesis": "SMT_summary",
-    "characterization": "CMT_summary",
-    "descriptor": "DSC_summary"}
-
 def abstracts_results_html(search_text):
     entity_query = parse_search_box(search_text)
-    results = rester.abstracts_search(entity_query, text=None,
-                                      top_k=MAX_N_ABSTRACTS)
+    results = rester.abstracts_search(
+        entity_query,
+        text=None,
+        top_k=MAX_N_ABSTRACTS
+    )
+
     if not results:
         return no_results()
     else:
@@ -34,48 +29,30 @@ def abstracts_results_html(search_text):
 
         df['authors'] = df['authors'].apply(format_authors)
 
-        formatted_results =
-        results = [format_result(df.iloc[i])
-                   for i in range(min(len(df), max_rows))]
+        n_formatted_results = min(len(df), MAX_N_ABSTRACTS)
+        formatted_results = [None] * n_formatted_results
+
+        if n_formatted_results > MAX_N_ABSTRACTS:
+            label_txt = \
+                f"Showing {n_formatted_results} of > {MAX_N_ABSTRACTS} " \
+                f"results. For full results, use the "
+            label_link = html.A(
+                'Matscholar API.',
+                href='https://github.com/materialsintelligence/matscholar'
+            )
+            label = html.Label([label_txt, label_link])
+        else:
+            label_txt = f"Showing all {n_formatted_results} results."
+            label = html.Label(label_txt)
+
+        for i in range(n_formatted_results):
+            formatted_results[i] = format_result(df.iloc[i])
+        paper_table = html.Table(formatted_results, className="table")
+
         return html.Div(
-            [html.Label(generate_nr_results(len(results)), id="number_results"),
-             html.Table(
-                 results,
-                 className=results_container_class())])
-
-
-
-def highlight_material(body, material):
-    highlighted_phrase = html.Mark(material)
-    if len(material) > 0 and material in body:
-        chopped = body.split(material)
-        newtext = []
-        for piece in chopped[:-1]:
-            newtext.append(piece)
-            newtext.append(highlighted_phrase)
-        newtext.append(chopped[-1])
-        return newtext
-    return body
-
-
-def generate_nr_results(n):
-    """
-    Generates a message to be displayed at top of results page.
-    Args:
-        n (int): Number of results returned.
-    Returns:
-        (str or list) Message to be displayed.
-    """
-    if n == 0:
-        return "No Results"
-    elif n >= MAX_N_ABSTRACTS:
-        return [
-            'Showing {} of > {:,} results. For full results, use the '.format(
-                MAX_N_ABSTRACTS, n),
-            html.A('Matscholar API.',
-                   href='https://github.com/materialsintelligence/matscholar')]
-    else:
-        return 'Showing {} of {:,} results'.format(min(MAX_N_ABSTRACTS, n), n)
+            [label, paper_table],
+            className=results_container_class()
+        )
 
 
 def format_result(result):
@@ -92,14 +69,16 @@ def format_result(result):
         html.Div of formatted result
     """
 
-    columns = ['title', 'authors', 'year', 'journal', 'abstract', ]
+    title_link = html.A(
+        result['title'],
+        href=result["link"],
+        target="_blank",
+    )
 
-    title = html.Div(html.A(result['title'],
-                            href=result["link"],
-                            target="_blank",
-                            style={"font-size": "120%"}
-                            )
-                     )
+    title = html.Div(
+        title_link,
+        className="is-size-4 has-text-info"
+    )
 
     # Format the 2nd line "authors - journal, year" with ellipses for overflow
     characters_remaining = 90  # max line length
@@ -127,21 +106,43 @@ def format_result(result):
         authors += "..."
 
     ajy = "{} - {}, {}".format(authors, journal, year)
-    authors_journal_and_year = html.Div(ajy, style={"color": "green"})
-    abstract = html.Div(result["abstract"])
+    authors_journal_and_year = html.Div(
+        ajy,
+        className="is-size-5 has-text-success"
+    )
+
+    abstract_txt = result["abstract"]
+    abstract = html.Div(abstract_txt, className="is-size-6")
+
+    label_mapping = {
+        "material": "MAT_summary",
+        "application": "APL_summary",
+        "property": "PRO_summary",
+        "phase": "SPL_summary",
+        "synthesis": "SMT_summary",
+        "characterization": "CMT_summary",
+        "descriptor": "DSC_summary"}
 
     entities = []
     for f in valid_entity_filters:
         for e in result[label_mapping[f]]:
-            entities.append(html.Span(e,
-                                      className="highlighted {}".format(
-                                          entity_shortcode_map[f])))
-    entities = html.Div(entities)
+            entity = html.Div(e, className="column is-narrow has-margin-5")
+            entities.append(entity)
 
-    return html.Tr(html.Td(html.Div([title,
-                                     authors_journal_and_year,
-                                     abstract,
-                                     entities])))
+    entities_label = html.Div(
+        "Extracted entities:",
+        className="column is-narrow has-margin-5"
+    )
+    entities = html.Div([entities_label] + entities, className="columns has-max-width-350")
+    entities_container = html.Div(entities, className="container")
+
+
+    paper_div = html.Div(
+        [title, authors_journal_and_year, abstract, entities_container]
+    )
+
+    table_cell = html.Td(paper_div)
+    return html.Tr(table_cell)
 
 
 def format_authors(author_list):
@@ -157,3 +158,16 @@ def format_authors(author_list):
             author_list.reverse()
             author_list = " ".join(author_list)
         return author_list
+
+
+def highlight_material(body, material):
+    highlighted_phrase = html.Mark(material)
+    if len(material) > 0 and material in body:
+        chopped = body.split(material)
+        newtext = []
+        for piece in chopped[:-1]:
+            newtext.append(piece)
+            newtext.append(highlighted_phrase)
+        newtext.append(chopped[-1])
+        return newtext
+    return body
