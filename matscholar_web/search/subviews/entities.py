fd8decd1fb84ad738c945a54a0c34b2781367a39
ardunn <ardunn@lbl.gov>
2019-09-24 19:09:09 -0700

    working and not terribly formatted entity tables, wip

--- matscholar_web/search/subviews/entities.py
+++ matscholar_web/search/subviews/entities.py
@@ -10,39 +10,63 @@ from matscholar_web.constants import rester, valid_entity_filters, \
 
 MAX_N_ROWS_FOR_EACH_ENTITY_TABLE = 10
 
-def gen_output(most_common, entity_type, class_name="three column"):
-    entity_shortcode = entity_shortcode_map[entity_type.lower()]
-    header_entity_type = html.Th(entity_type, className="highlighted {}".format(entity_shortcode))
-    header_score = html.Th("score", className="highlighted {}".format(entity_shortcode))
+def get_score_table_for_entity(most_common, entity_type, width):
+    n_results = len(most_common)
+    header_entity_type = html.Th(f"{entity_type}: ({n_results} entities)")
+    header_score = html.Th("score", className="has-width-50")
     header = html.Tr([header_entity_type, header_score])
 
-    rows = [None] * len(most_common)
+    rows = [None] * n_results
 
     row_number = 0
     for ent, count, score in most_common:
-        entity = html.Td(ent)
-        score = html.Td('{:.2f}'.format(score))
+        entity = html.Td(ent, className="has-width-50")
+        score = html.Td('{:.2f}'.format(score), className="has-width-50")
         rows[row_number] = html.Tr([entity, score])
         row_number += 1
         if row_number == MAX_N_ROWS_FOR_EACH_ENTITY_TABLE - 1:
             break
+    table = html.Table([header] + rows, className="table is-fullwidth is-bordered is-hoverable is-narrow is-striped")
+    return html.Div(table, className=f"column {width}")
 
-    table = html.Table([header] + rows)
-    return html.Div(table)
 
+def get_all_score_tables(results_dict):
+    columns_classes = "columns is-desktop is-centered"
 
-def gen_table(results_dict):
-    return html.Div([
-        html.Div([
-            gen_output(results_dict["PRO"], "Property"),
-            gen_output(results_dict["APL"], "Application"),
-            gen_output(results_dict["CMT"], "Characterization"),
-            gen_output(results_dict["SMT"], "Synthesis")]),
-        html.Div([
-            gen_output(results_dict["DSC"], "Descriptor"),
-            gen_output(results_dict["SPL"], "Phase"),
-            gen_output(results_dict["MAT"], "Material")]),
-    ])
+    half = "is-one-half"
+    third = "is-one-third"
+
+    row1 = html.Div(
+        [
+            get_score_table_for_entity(results_dict["PRO"], "Property", half),
+            get_score_table_for_entity(results_dict["APL"], "Application", half),
+        ],
+        className=columns_classes
+    )
+
+    row2 = html.Div(
+        [
+            get_score_table_for_entity(results_dict["CMT"], "Characterization", half),
+            get_score_table_for_entity(results_dict["SMT"], "Synthesis", half),
+        ],
+        className=columns_classes
+    )
+
+    row3 = html.Div(
+        [
+            get_score_table_for_entity(results_dict["DSC"], "Descriptor", third),
+            get_score_table_for_entity(results_dict["SPL"], "Phase", third),
+            get_score_table_for_entity(results_dict["MAT"], "Material", third)
+        ],
+        className=columns_classes
+    )
+
+    row4 = html.Div(
+        [
+        ],
+        className=columns_classes
+    )
+    return html.Div([row1, row2, row3, row4])
 
 
 
@@ -85,4 +109,4 @@ def entities_results_html(n_clicks, dropdown_value, search_text):
         no_results_container = html.Div(no_results, className="columns is-centered")
         return no_results_container
     else:
-        return gen_table(results)
+        return get_all_score_tables(results)
