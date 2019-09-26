46d2792ce0680f9d71756bf8e703a67d418d3052
ardunn <ardunn@lbl.gov>
2019-09-25 17:32:16 -0700

    fix triple no results found on everything view

--- matscholar_web/search/subviews/everything.py
+++ matscholar_web/search/subviews/everything.py
@@ -3,20 +3,34 @@ import dash_html_components as html
 from matscholar_web.search.subviews.abstracts import abstracts_results_html
 from matscholar_web.search.subviews.entities import entities_results_html
 from matscholar_web.search.subviews.materials import materials_results_html
+from matscholar_web.search.util import no_results_html
 
 
 def everything_results_html(search_text):
 
-    scroll_down = html.Label("Scroll down for more!", className="is-size-4 has-margin-10 has-margin-bottom-20")
+    scroll_down = html.Label(
+        "Scroll down for more!",
+        className="is-size-4 has-margin-10 has-margin-bottom-20"
+    )
     scroll_down_container = html.Div(scroll_down)
 
-    container = html.Div(
-        [
-            scroll_down_container,
-            entities_results_html(search_text),
-            materials_results_html(search_text),
-            abstracts_results_html(search_text),
-         ],
-        className="container"
-    )
-    return container
+    entities_results = entities_results_html(search_text)
+    materials_results = materials_results_html(search_text)
+    abstracts_results = abstracts_results_html(search_text)
+    all_results = [entities_results, materials_results, abstracts_results]
+
+    no_results = no_results_html()
+
+    if all([str(r) == str(no_results) for r in all_results]):
+        return no_results
+    else:
+        container = html.Div(
+            [
+                scroll_down_container,
+                entities_results,
+                materials_results,
+                abstracts_results,
+            ],
+            className="container"
+        )
+        return container
