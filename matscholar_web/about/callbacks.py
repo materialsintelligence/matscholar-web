55a92e88b75cae3058fccaabedfd65039ce53360
ardunn <ardunn@lbl.gov>
2019-09-26 14:35:59 -0700

    update funding

--- matscholar_web/about/callbacks.py
+++ matscholar_web/about/callbacks.py
@@ -3,4 +3,5 @@ from matscholar_web.constants import rester
 
 def get_n_abstracts():
     current_n_abstracts = "{:0,.0f}".format(rester.get_abstract_count())
+    print(current_n_abstracts)
     return current_n_abstracts
--- matscholar_web/about/view.py
+++ matscholar_web/about/view.py
@@ -68,6 +68,17 @@ def get_introduction():
         "faster! We are actively working on our infrastructure to make " \
         "Matscholar faster, more accurate, and more informative."
 
+
+    funding_header_txt = "Our funding sources"
+    funding_body_txt = \
+        "Matscholar is supported by Toyota Research Institute through the " \
+        "Accelerated Materials Design and Discovery program. Abstract data " \
+        "was downloaded from the ScienceDirect API between October 2017 and " \
+        "September 2018, and the Scopus API in July 2019 via " \
+        "http://api.elsevier.com and http://www.scopus.com. Abstract " \
+        "data was also downloaded from the SpringerNature API and Royal " \
+        "Society of Chemistry sources."
+
     current_abstracts = html.Div(
         id="n-current-abstracts",
         className="is-size-2 has-margin-10 has-text-centered has-text-weight-bold"
@@ -78,6 +89,7 @@ def get_introduction():
     why_use_body_md = dcc.Markdown(why_use_body_txt)
     reference_1_md = dcc.Markdown(reference_1_txt)
     reference_2_md = dcc.Markdown(reference_2_txt)
+    funding_md = dcc.Markdown(funding_body_txt)
 
     body_style = "is-size-6-desktop has-margin-5"
     header_style = "is-size-5-desktop has-text-weight-bold has-margin-10"
@@ -97,6 +109,8 @@ def get_introduction():
     why_use_header = html.Div(why_use_header_txt, className=header_style)
     why_use_body = html.Div(why_use_body_md, className=body_style)
     why_use_body_container = html.Div(why_use_body, className="has-margin-right-40 has-margin-left-40 has-margin-top-5 has-margin-bottom-5")
+    funding_header = html.Div(funding_header_txt, className=header_style)
+    funding_body = html.Div(funding_md, className=body_style)
 
     introduction_box = html.Div(
         [
@@ -108,7 +122,9 @@ def get_introduction():
             reference_1,
             reference_2,
             why_use_header,
-            why_use_body_container
+            why_use_body_container,
+            funding_header,
+            funding_body
         ],
         className="box has-padding-200"
     )
