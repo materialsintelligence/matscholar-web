7c069052e194734929e2eafc4127f19c2175109d
ardunn <ardunn@lbl.gov>
2019-09-28 23:03:12 -0700

    make about page at least show placeholder number of abstracts if rester refuses to give an answer

--- matscholar_web/about/callbacks.py
+++ matscholar_web/about/callbacks.py
@@ -2,11 +2,15 @@ from matscholar_web.constants import rester
 
 
 def get_n_abstracts():
-    count = rester.get_abstract_count()
+
+    try:
+        count = rester.get_abstract_count()
+    except:
+        count = 0
 
     # take care of rester error in the meantime
     if count == 0:
-        count = 3000000
+        count = 5000000
 
     current_n_abstracts = "{:0,.0f}".format(count)
     return current_n_abstracts
