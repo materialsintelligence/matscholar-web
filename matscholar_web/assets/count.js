fef75737e26aa00d7349e146f65c9c1d825a9d2e
ardunn <ardunn@lbl.gov>
2019-10-04 20:11:21 -0700

    try regex for formatting live update stats

--- matscholar_web/assets/count.js
+++ matscholar_web/assets/count.js
@@ -1,10 +1,12 @@
 function animateValue(id, countTo, duration) {
-    // assumes integer values for start and end
+    var radix = 10
+    var obj = document.getElementById(id);
+    var countTo = parseInt(obj, radix)
 
+    // assumes integer values for start and end
     var start = 0.75 * countTo;
     var end = countTo;
 
-    var obj = document.getElementById(id);
     var range = end - start;
     // no timer shorter than 50ms (not really visible any way)
     var minTimer = 50;
@@ -23,7 +25,7 @@ function animateValue(id, countTo, duration) {
         var now = new Date().getTime();
         var remaining = Math.max((endTime - now) / duration, 0);
         var value = Math.round(end - (remaining * range));
-        obj.innerHTML = parseInt(value, 10);
+        obj.innerHTML = value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
         if (value == end) {
             clearInterval(timer);
         }
@@ -34,3 +36,5 @@ function animateValue(id, countTo, duration) {
 }
 
 window.setTimeout(animateValue, 1000, "jscount-materials", 298616, 5000);
+window.setTimeout(animateValue, 1000, "jscount-entities", 298616, 5000);
+window.setTimeout(animateValue, 1000, "jscount-abstracts", 298616, 5000);
