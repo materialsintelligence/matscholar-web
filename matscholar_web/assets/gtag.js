80dbe89df3f3de93f6a33793120b1de249f63e9f
ardunn <ardunn@lbl.gov>
2019-10-04 18:29:52 -0700

    update google analytics, hopefully fixes #129

--- matscholar_web/assets/gtag.js
+++ matscholar_web/assets/gtag.js
@@ -1,10 +1,7 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-149443072-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
 
-  gtag('config', 'UA-149443072-1');
+gtag('config', 'UA-149443072-1');
 
 
