59ef44123202ad83e2fd23c8624b5e5ad71023d2
ardunn <ardunn@lbl.gov>
2019-10-06 15:37:48 -0700

    wip example searches 2

--- matscholar_web/assets/example_searches.js
+++ matscholar_web/assets/example_searches.js
@@ -1,12 +1,23 @@
 function runExampleSearches(id, exampleSearchesStringId, rotationTime){
     var obj = document.getElementById(id);
     var exampleSearchesString = document.getElementById(exampleSearchesStringId).innerHTML;
-    var exampleSearches = exampleSearchesString.split();
-    for (i = 0; i < 1000; i++){
-        var internalIndex = i % exampleSearches
-        setTimeout(function(){
-            console.log
-            obj.innerHTML = exampleSearches[internalIndex]
-        })
-    }
-}
+    var exampleSearches = exampleSearchesString.split(" | ");
+
+    console.log(exampleSearches)
+    console.log(exampleSearches[0])
+
+    for (let i = 0; i < 1000; i++){
+        (function (i) {
+            var internalIndex = i % exampleSearches.length;
+            console.log(internalIndex)
+            setTimeout(
+                function(){
+                    var exampleSearch = exampleSearches[internalIndex];
+                    console.log(exampleSearch);
+                    obj.innerHTML = exampleSearch;
+                },
+                rotationTime
+            );
+        })(i);
+    };
+};
