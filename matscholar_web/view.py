ee9c218bc10cf1edaa9ab17f10ebd541ff5f2b80
ardunn <ardunn@lbl.gov>
2019-10-24 12:45:56 -0700

    update the core view

--- matscholar_web/view.py
+++ matscholar_web/view.py
@@ -22,11 +22,15 @@ def core_view_html():
         core_view (dash_html_components): An html block for the entire site.
 
     """
-    footer_interior = footer_html()
+    # The nav bar for all apps
     nav = nav_html()
+
+    # The footer for all apps
+    footer_interior = footer_html()
     footer_section = html.Div(footer_interior, className="section")
     footer = html.Footer(footer_section, className="footer has-margin-top-50")
 
+    # The container for individual apps
     app_container = html.Div("", id="app_container",
                              className="container is-fluid")
     app_expander = html.Div(app_container, className="msweb-is-tall")
@@ -35,6 +39,7 @@ def core_view_html():
         className="msweb-is-tall-container msweb-fade-in has-margin-top-50"
     )
 
+    # External stylesheets should be linked here.
     external_stylesheets = \
         html.Link(
             href="https://fonts.googleapis.com/css?family=Ubuntu&display=swap",
@@ -42,6 +47,7 @@ def core_view_html():
             className="is-hidden"
         )
 
+    # Location defines the linking for how to change the app.
     location = dcc.Location(id="url", refresh=False)
 
     core_view = html.Div(
