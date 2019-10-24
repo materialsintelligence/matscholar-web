318c119cbd298698c230e1ee2c93efc8d7d1ac31
ardunn <ardunn@lbl.gov>
2019-10-24 14:10:23 -0700

    placeholder for search tests

---
+++ matscholar_web/search/tests/util.py
@@ -0,0 +1,13 @@
+#     An ultra-minimal example set is:
+#     example_searches = [
+#         # "blahblah", # should fail
+#         # "traw: Zintl", # should fail
+#         "phase: diamond, application: thermoelectric, descriptor: thin film",
+#         "application: thermoelectric, phase: diamond",
+#         "phase: diamond application: thermoelectric",
+#         "phase: diamond, heusler application: thermoelectric",
+#         "phase: diamond, heusler, application: thermoelectric",
+#         # "phase: diamond, phase: heusler",  # should fail
+#         "characterization: x-ray diffraction, EDS, material: Pb"
+#         "Application: thermoelectric CharaCterizAtion: x-ray diffraction material: PbTe"
+#     ]
