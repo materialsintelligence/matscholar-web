f90fe93dbf75d5d041637290c45e27e6ffcd3a1a
ardunn <ardunn@lbl.gov>
2019-10-24 21:01:23 -0700

    initiate some tests to be dealt with later

---
+++ matscholar_web/search/subviews/tests/test_everything.py
@@ -0,0 +1,15 @@
+from matscholar_web.tests.util import MatScholarWebBaseTest
+
+import matscholar_web.search.subviews.everything as msweb_ssev
+
+"""
+Tests for the abstracts subview of search.
+"""
+
+# Functions to exclude from this test
+EXCLUDE = []
+
+
+# class TestSearchEverythingSubview(MatScholarWebBaseTest):
+#     def test_everything_subviews(self):
+#         self.run_test_for_all_functions_in_module(msweb_ssev, EXCLUDE)
