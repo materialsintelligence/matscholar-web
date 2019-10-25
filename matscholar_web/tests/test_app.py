5bdb6957680473b33ccf6c0ce86437bdbac622fe
ardunn <ardunn@lbl.gov>
2019-10-24 20:55:49 -0700

    add tests for the main app

--- matscholar_web/tests/test_app.py
+++ matscholar_web/tests/test_app.py
@@ -1,3 +1,5 @@
+import dash_html_components as html
+
 from matscholar_web.tests.util import MatScholarWebBaseTest
 from matscholar_web.app import display_app_html
 
@@ -5,12 +7,12 @@ from matscholar_web.app import display_app_html
 Tests for callbacks in the main app which are not tested elsewhere.
 """
 
-# Functions to exclude from this test
-EXCLUDE = ["guided_search_box_elastic_html"]
-
 
 class TestCoreAppCallbacks(MatScholarWebBaseTest):
     def test_display_app(self):
         f = display_app_html
         arg_combos = [(a, ) for a in ["/", "/search", "/about", "/extract", "/journals"]]
-        self.run_test_for_individual_arg_combos(f, arg_combos)
+        valid_types = (str, dict, html.Div)
+        for arg_combo in arg_combos:
+            o = f(*arg_combo)
+            self.assertTrue(isinstance(o, valid_types))
