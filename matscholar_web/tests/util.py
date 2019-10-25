f5909aa70606bfebabab1031ef0668036c0e6bcd
ardunn <ardunn@lbl.gov>
2019-10-24 20:11:21 -0700

    update util

--- matscholar_web/tests/util.py
+++ matscholar_web/tests/util.py
@@ -3,35 +3,26 @@ import unittest
 
 import dash_html_components as html
 
-
 """
 Utilities for running tests.
 """
 
-
-def get_all_functions_in_module(module):
-    """
-    Get all the functions in a module.
-
-    Will only get functions defined _in that module_! If they are imported
-    from elsewhere they will not be fetched!
-
-    Args:
-        module (python module): The module.
-
-    Returns:
-        functions ({str: function}): A dictionary of function names and the
-            first-class function objects.
-    """
-    name_func_tuples = inspect.getmembers(module, inspect.isfunction)
-    name_func_tuples = [t for t in name_func_tuples if
-                        inspect.getmodule(t[1]) == module]
-    functions = dict(name_func_tuples)
-    return functions
-
-
 class MatScholarWebBaseTest(unittest.TestCase):
     def run_test_for_all_functions_in_module(self, module, exclude):
+        """
+        Run a test for every function in a module.
+
+        Should only be run on components returning html blocks or their
+        styling elements.
+
+        Args:
+            module (python module): the python module with functions to test
+            exclude ([str]): A list of function names as strings to exclude
+                from testing.
+
+        Returns:
+            None
+        """
         functions = get_all_functions_in_module(module)
         for fname, f in functions.items():
             if fname in exclude:
@@ -43,13 +34,13 @@ class MatScholarWebBaseTest(unittest.TestCase):
                 n_args = len(params)
                 if n_args == 0:  # this function takes no args
                     o = f()
-                    self._run_basic_type_checker_on_object(fname, o)
+                    self._basic_type_check_on_function(fname, o)
                 else:
                     fake_args = ["arg"] * n_args
                     o = f(*fake_args)
-                    self._run_basic_type_checker_on_object(fname, o)
+                    self._basic_type_check_on_function(fname, o)
 
-    def _run_basic_type_checker_on_function_object(self, fname, o):
+    def _basic_type_check_on_function(self, fname, o):
         """
         If "_html" is in the name, make sure the function returns an html.Div
         on the function object.
@@ -73,3 +64,24 @@ class MatScholarWebBaseTest(unittest.TestCase):
             self.assertTrue(isinstance(o, str))
         else:
             self.assertFalse(isinstance(o, html.Div))
+
+
+def get_all_functions_in_module(module):
+    """
+    Get all the functions in a module.
+
+    Will only get functions defined _in that module_! If they are imported
+    from elsewhere they will not be fetched!
+
+    Args:
+        module (python module): The module.
+
+    Returns:
+        functions ({str: function}): A dictionary of function names and the
+            first-class function objects.
+    """
+    name_func_tuples = inspect.getmembers(module, inspect.isfunction)
+    name_func_tuples = [t for t in name_func_tuples if
+                        inspect.getmodule(t[1]) == module]
+    functions = dict(name_func_tuples)
+    return functions
