5003139412c78805bcd49e4a1b594d32cc7b7c3c
John Dagdelen <jdagdelen@users.noreply.github.com>
2020-01-16 11:18:08 -0800

    Update logic.py

--- matscholar_web/search/logic.py
+++ matscholar_web/search/logic.py
@@ -67,8 +67,8 @@ def show_search_results(go_button_n_clicks, dropdown_value, search_text):
 #                 "undergoing maintenance, please visit again later!"
 #             )
             rester_error = (
-                "Our search functionality is currently down for scheduled maintenence. "
-                "Please try again after January 16th, 2020. "
+                "Our server is having trouble making a suggestion for that "
+                "abstract. We are likely undergoing maintenance, check back soon!"
             )
             return common_rester_error_html(rester_error)
 
